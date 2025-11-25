58cfb19240a09055397cf36c2679ce4362c66d71
Jedrick Ocenar <jdpocenar@mymail.mapua.edu.ph>
2025-11-25 13:02:10 +0800

    added textprinter

--- Main.java
+++ Main.java
@@ -24,23 +24,7 @@ public class Main {
         PropertyMenuManager propertyMenu = new PropertyMenuManager(system, helper, calendarView);
         BookingMenuManager bookingMenu = new BookingMenuManager(system, helper);
 
-        boolean running = true;
-
-        while (running) {
-            System.out.println("\n===== GREEN PROPERTY EXCHANGE =====");
-            System.out.println("1. Property Menu");
-            System.out.println("2. Booking Menu");
-            System.out.println("0. Exit");
-
-            int choice = helper.getValidInt("Enter choice: ", 0, 2);
-
-            if (choice == 0)
-                running = false;
-            else if (choice == 1)
-                propertyMenu.openPropertyMenu();
-            else if (choice == 2)
-                bookingMenu.openBookingMenu();
-        }
+        TextPrinter.initProgram(true, helper, propertyMenu, bookingMenu);
 
         System.out.println("Exiting system... Goodbye!");
         scanner.close();
---
+++ TextPrinter.java
@@ -0,0 +1,20 @@
+
+public class TextPrinter {
+    public static void initProgram(boolean running, MenuHelper helper, PropertyMenuManager propertyMenu, BookingMenuManager bookingMenu){
+        while (running) {
+            System.out.println("\n===== GREEN PROPERTY EXCHANGE =====");
+            System.out.println("1. Property Menu");
+            System.out.println("2. Booking Menu");
+            System.out.println("0. Exit");
+
+            int choice = helper.getValidInt("Enter choice: ", 0, 2);
+
+            if (choice == 0)
+                running = false;
+            else if (choice == 1)
+                propertyMenu.openPropertyMenu();
+            else if (choice == 2)
+                bookingMenu.openBookingMenu();
+        }
+    }
+}
